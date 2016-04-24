91599b5be2f69af6adf49b281f0a941228b47079
Cameron Taggart <cameron@tachyus.com>
2016-04-24 00:08:11 -0700

    removed mountin at /home/vscode since it makes the extensions disapear

--- run-dockerformac.sh
+++ run-dockerformac.sh
@@ -3,4 +3,4 @@ open -a XQuartz
 xhost + # TODO limit to just the host
 # bridge100 is disappearing after a reboot
 ip=`ifconfig bridge100 | grep inet | awk '$1=="inet" {print $2}'`
-docker run --rm -it -w /root -e DISPLAY=$ip:0 -v $PWD:/home/vscode ctaggart/golang-vscode
+docker run --rm -it -w /root -e DISPLAY=$ip:0 ctaggart/golang-vscode
