690167b717c231de5eba4f176ab8c9955f778d92
Cameron Taggart <cameron@tachyus.com>
2016-04-30 16:13:16 -0700

    security-opt on own line

--- run-dockerformac.sh
+++ run-dockerformac.sh
@@ -4,7 +4,8 @@ xhost + # TODO limit to just the host
 # bridge100 is disappearing after a reboot
 ip=`ifconfig bridge100 | grep inet | awk '$1=="inet" {print $2}'`
 docker run --rm -it \
-    -e DISPLAY=$ip:0 --security-opt seccomp=unconfined \
+    -e DISPLAY=$ip:0 \
+    --security-opt seccomp=unconfined \
     -v `pwd`:/home/vscode/golang-gscode \
     ctaggart/golang-vscode \
     su - vscode -c "cd ~/golang-gscode; code -w ."
--- run-nut.sh
+++ run-nut.sh
@@ -4,7 +4,8 @@ xhost + # TODO limit to just the host
 # bridge100 is disappearing after a reboot
 ip=`ifconfig bridge100 | grep inet | awk '$1=="inet" {print $2}'`
 docker run --rm -it \
-    -e DISPLAY=$ip:0 --security-opt seccomp=unconfined \
+    -e DISPLAY=$ip:0 \
+    --security-opt seccomp=unconfined \
     -v $HOME/go/nut:/home/vscode/nut \
     ctaggart/golang-vscode \
     su - vscode -c "cd ~/nut; ./vscode.sh"
